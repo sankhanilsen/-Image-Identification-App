0b37fe7a4ecc727cc0d7404d4fa57178e379f346
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-04-16 15:49:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot(){
 }
 console.log('ml5 version', ml5.version);
 
-classifier=ml5.imageClassfier('MobileNet', modelLoaded);
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 function modelLoaded(){
     console.log('model loaded :) !');
 }
